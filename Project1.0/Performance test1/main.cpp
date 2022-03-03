8aa4b92c8cbab23842e0a0c3944fe8018510e81a
Donny <tpc1789883718@163.com>
2022-03-03 17:25:24 +0800

    2.0

--- Project1.0/Performance test1/main.cpp
+++ Project1.0/Performance test1/main.cpp
@@ -5,20 +5,45 @@ using namespace std;
 
 int main()
 {
-    int matrix[5][5] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
-    int vec[5] = {10,20,30,40,50};
-    int result[5] = {0};
+    int n = 0;
+    cin >> n;
+    float* a = new float[n];
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+    }
+    float** b = new float*[n];
+    for(int i=0;i<n;i++){
+        b[i] = new float[n];
+        for(int j=0;j<n;j++){
+            cin >> b[i][j];
+        }
+    }
+    float* sum = new float[n];
     long long head, tail, freq;
     QueryPerformanceFrequency((LARGE_INTEGER *)&freq);
     QueryPerformanceCounter((LARGE_INTEGER *)&head);
-    //ÖðÁÐ¼ÆËã
-    for(int i=0;i<5;i++){
-        for(int j=0;j<5;j++){
-            result[i] += matrix[j][i] * vec[j];
+    for(int i=0;i<n;i++){
+        sum[i] = 0.0;
+        for(int j=0;j<n;j++){
+            sum[i] += b[j][i] * a[j];
         }
-        cout << "col " << i+1 << ":" << result[i] << endl;
     }
     QueryPerformanceCounter((LARGE_INTEGER *)&tail);
     cout << (tail - head) * 1000.0 / freq << "ms" << endl;
+
+    float* sum_ = new float[n];
+    long long head_, tail_, freq_;
+    QueryPerformanceFrequency((LARGE_INTEGER *)&freq_);
+    QueryPerformanceCounter((LARGE_INTEGER *)&head_);
+    for(int i=0;i<n;i++){
+        sum_[i] = 0.0;
+    }
+    for(int j = 0; j < n; j++){
+        for(int i = 0; i < n; i++){
+            sum_[i] += b[j][i] * a[j];
+        }
+    }
+    QueryPerformanceCounter((LARGE_INTEGER *)&tail_);
+    cout << (tail_ - head_) * 1000.0 / freq_ << "ms" << endl;
     return 0;
 }
